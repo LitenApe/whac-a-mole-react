4fd5a15f378bf1a8ba8a6233434fd51ebce3e7d9
Son Thanh Vo <sonvo95@outlook.com>
2021-06-24 19:46:10 +0200

    refactor: remove unnecessary array

--- src/components/Statistics.ts
+++ src/components/Statistics.ts
@@ -16,15 +16,21 @@ export default function Statistics(props: StatisticsProps) {
   return createElement(
     'table',
     rest,
-    createElement('tbody', null, [
-      createElement('tr', { key: 'tr-hits' }, [
+    createElement(
+      'tbody',
+      null,
+      createElement(
+        'tr',
+        { key: 'tr-hits' },
         createElement('td', { key: 'td-hits-label' }, 'Hits:'),
-        createElement('td', { key: 'td-hits-data' }, hit),
-      ]),
-      createElement('tr', { key: 'tr-miss' }, [
+        createElement('td', { key: 'td-hits-data' }, hit)
+      ),
+      createElement(
+        'tr',
+        { key: 'tr-miss' },
         createElement('td', { key: 'td-miss-label' }, 'Miss:'),
-        createElement('td', { key: 'td-miss-data' }, miss),
-      ]),
-    ])
+        createElement('td', { key: 'td-miss-data' }, miss)
+      )
+    )
   );
 }
